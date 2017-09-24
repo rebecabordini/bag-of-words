baab6cc660683b981dda6b6f50c402aea5c278c3
luisaRosa <luisasilveirarosa@gmail.com>
2017-09-24 14:23:03 -0300

    Add files via upload

---
+++ models/bag_of_words.py
@@ -0,0 +1,15 @@
+from sklearn.feature_extraction.text import CountVectorizer
+
+class BagOfWords:
+    def __init__(self, documents):
+        self.documents = documents
+        self.bag_of_words = self.generate_bag_of_words()
+
+    def generate_bag_of_words(self):
+        doc=[]
+
+        for document in self.documents:
+            doc.append(document.body)
+
+        vectorizer = CountVectorizer()
+        return vectorizer.fit_transform(doc).todense()
